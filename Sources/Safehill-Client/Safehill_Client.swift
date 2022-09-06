e114b2f6cbedf8e359a5ba812b370bf9f4ad1e84
gennaro-safehill <95962467+gennaro-safehill@users.noreply.github.com>
2022-09-06 02:18:39 -0700

    Lower hi res definition

--- Sources/Safehill-Client/Safehill_Client.swift
+++ Sources/Safehill-Client/Safehill_Client.swift
@@ -4,12 +4,12 @@ import os
 internal let log = Logger(subsystem: "com.gf.safehill", category: "SafehillClient")
 
 public let kSHLowResPictureSize = CGSize(width: 480.0, height: 480.0)
-public let kSHHiResPictureSize = CGSize(width: 2048.0, height: 2048.0)
+public let kSHHiResPictureSize = CGSize(width: 1920.0, height: 1920.0)
 public let kSHFullResPictureSize: CGSize? = nil
 
 
 /* Set this to true to randomly simulate failures */
-let kSHSimulateBackgroundOperationFailures = true
+let kSHSimulateBackgroundOperationFailures = false
 
 
 public enum SHBackgroundOperationError : Error {
