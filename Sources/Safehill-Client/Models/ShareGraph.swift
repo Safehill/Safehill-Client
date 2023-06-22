ef384c6315697595d6e16aa0c70cbb6de3e44df4
gennaro-safehill <gennaro@safehill.com>
2023-06-22 03:36:32 -0700

    KBKVStore can not be force casted to a KBKnowledgeStore

--- Sources/Safehill-Client/Models/ShareGraph.swift
+++ Sources/Safehill-Client/Models/ShareGraph.swift
@@ -11,7 +11,10 @@ public struct SHShareGraph {
     
     init() {
         do {
-            self.store = try KBKnowledgeStore.initDBHandlerWithRetries(dbName: "com.gf.safehill.KnowledgeGraph") as! KBKnowledgeStore
+            let kvStore = try KBKVStore.initDBHandlerWithRetries(dbName: "com.gf.safehill.KnowledgeGraph")
+            /// Since we could initialize a handler to connect to the DB on this location
+            /// it should be safe to force initialize the KBKnowledgeStore at this point
+            self.store = KBKnowledgeStore.store(kvStore.location)!
         } catch {
             fatalError("knowledge graph database handler could not be initialized")
         }
