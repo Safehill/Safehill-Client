d50d641232e2b5420fb18ada9796b9984fff935a
gennaro-safehill <gennaro@safehill.com>
2023-07-17 05:35:01 -0700

    Known users are users that have any asset shared with this user

--- Sources/Safehill-Client/Models/ShareGraph.swift
+++ Sources/Safehill-Client/Models/ShareGraph.swift
@@ -10,12 +10,8 @@ public enum SHKGQuery {
     static func isKnownUser(withIdentifier userId: String) throws -> Bool {
         let graph = try SHDBManager.sharedInstance.graph()
         let userEntity = graph.entity(withIdentifier: userId)
-        let linkingCount = try userEntity.linkingEntities().count
-        if linkingCount > 0 {
-            return true
-        }
-        let linkedCount = try userEntity.linkedEntities().count
-        if  linkedCount > 0 {
+        let sharesCount = try userEntity.linkingEntities(withPredicate: SHKGPredicates.shares.rawValue).count
+        if sharesCount > 0 {
             return true
         }
         return false
