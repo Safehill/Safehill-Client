a2308f49f617279d9470733590cb2633ecd88df1
gennaro-safehill <gennaro@safehill.com>
2023-09-05 22:23:02 -0700

    Prefixing logs with `[auth] `

--- Sources/Safehill-Client/Network/HTTPAPI.swift
+++ Sources/Safehill-Client/Network/HTTPAPI.swift
@@ -358,7 +358,7 @@ struct SHServerHTTPAPI : SHServerAPI {
                 ),
                       let authSalt = Data(base64Encoded: authChallenge.protocolSalt)
                 else {
-                    log.error("failed to decode challenge parameters")
+                    log.error("[auth] failed to decode challenge parameters")
                     return completionHandler(.failure(SHHTTPError.ServerError.unexpectedResponse("publicKey=\(authChallenge.publicKey) publicSignature=\(authChallenge.publicSignature) salt=\(authChallenge.protocolSalt)")))
                 }
                 
@@ -387,17 +387,17 @@ struct SHServerHTTPAPI : SHServerAPI {
                     self.post("signin/challenge/verify", parameters: parameters, requiresAuthentication: false) {
                         (result: Result<SHAuthResponse, Error>) in
                         if case .failure(let error) = result {
-                            log.error("failed to get verify auth challenge \(error.localizedDescription)")
+                            log.error("[auth] failed to get verify auth challenge \(error.localizedDescription)")
                         }
                         completionHandler(result)
                     }
                 }
                 catch {
-                    log.error("failed solve the auth challenge \(error.localizedDescription)")
+                    log.error("[auth] failed solve the auth challenge \(error.localizedDescription)")
                     completionHandler(.failure(error))
                 }
             case .failure(let err):
-                log.error("failed to get a new auth challenge from the server \(err.localizedDescription)")
+                log.error("[auth] failed to get a new auth challenge from the server \(err.localizedDescription)")
                 completionHandler(.failure(err))
             }
         }
