7e1631cb8f5c5f367aea935d1e8821d95cc05773
gennaro-safehill <gennaro@safehill.io>
2023-10-11 10:26:40 -0700

    Adding caching layer in the ShareGraph

--- Sources/Safehill-Client/Models/ShareGraph.swift
+++ Sources/Safehill-Client/Models/ShareGraph.swift
@@ -6,18 +6,26 @@ public enum SHKGPredicates: String {
     case sharedWith = "sharedWith"
 }
 
+var UserIdToAssetGidSharedByCache = [String: [GlobalIdentifier]]()
+var UserIdToAssetGidSharedWithCache = [String: [GlobalIdentifier]]()
+
 public enum SHKGQuery {
     public static func isKnownUser(withIdentifier userId: String) throws -> Bool {
-        let graph = try SHDBManager.sharedInstance.graph()
-        let userEntity = graph.entity(withIdentifier: userId)
-        let sharesCount = try userEntity.linkedEntities(withPredicate: SHKGPredicates.shares.rawValue).count
-        if sharesCount > 0 {
+        if let assetIdsSharedBy = UserIdToAssetGidSharedByCache[userId],
+           let assetIdsSharedWith = UserIdToAssetGidSharedWithCache[userId] {
+            return assetIdsSharedBy.count + assetIdsSharedWith.count > 0
+        }
+        
+        let assetIdsSharedBy = try SHKGQuery.assetGlobalIdentifiers(sharedBy: [userId])
+        if assetIdsSharedBy.count > 0 {
             return true
         }
-        let sharedWithCount = try userEntity.linkingEntities(withPredicate: SHKGPredicates.sharedWith.rawValue).count
-        if sharedWithCount > 0 {
+        
+        let assetIdsSharedWith = try SHKGQuery.assetGlobalIdentifiers(sharedWith: [userId])
+        if assetIdsSharedWith.count > 0 {
             return true
         }
+        
         return false
     }
     
@@ -52,12 +60,23 @@ public enum SHKGQuery {
         do {
             let kgAsset = graph.entity(withIdentifier: assetIdentifier)
             try kgSender.link(to: kgAsset, withPredicate: SHKGPredicates.shares.rawValue)
+            if let _ = UserIdToAssetGidSharedByCache[senderUserId] {
+                UserIdToAssetGidSharedByCache[senderUserId]!.append(assetIdentifier)
+            } else {
+                UserIdToAssetGidSharedByCache[senderUserId] = [assetIdentifier]
+            }
             for userId in receiverUserIds {
                 if userId == senderUserId {
                     continue
                 }
                 let kgOtherUser = graph.entity(withIdentifier: userId)
                 try kgAsset.link(to: kgOtherUser, withPredicate: SHKGPredicates.sharedWith.rawValue)
+                
+                if let _ = UserIdToAssetGidSharedWithCache[receiverUserIds] {
+                    UserIdToAssetGidSharedWithCache[receiverUserIds]!.append(assetIdentifier)
+                } else {
+                    UserIdToAssetGidSharedWithCache[receiverUserIds] = [assetIdentifier]
+                }
             }
         } catch {
             log.critical("[KG] failed to ingest descriptor for assetGid=\(assetIdentifier) into the graph")
@@ -90,7 +109,26 @@ public enum SHKGQuery {
         var assetsToUsers = [GlobalIdentifier: Set<String>]()
         var sharedByUsersCondition = KBTripleCondition(value: false)
         
+        var usersIdsToSearch = Set<String>()
         for userId in userIdentifiers {
+            if let assetIdsSharedBy = UserIdToAssetGidSharedByCache[userId] {
+                assetIdsSharedBy.forEach({
+                    if assetsToUsers[$0] == nil {
+                        assetsToUsers[$0] = [userId]
+                    } else {
+                        assetsToUsers[$0]!.insert(userId)
+                    }
+                })
+            } else {
+                usersIdsToSearch.insert(userId)
+            }
+        }
+        
+        guard usersIdsToSearch.count > 0 else {
+            return assetsToUsers
+        }
+        
+        for userId in usersIdsToSearch {
             sharedByUsersCondition = sharedByUsersCondition.or(
                 KBTripleCondition(
                     subject: userId,
@@ -107,6 +145,12 @@ public enum SHKGQuery {
             } else {
                 assetsToUsers[assetId] = [triple.subject]
             }
+            
+            if let _ = UserIdToAssetGidSharedByCache[triple.subject] {
+                UserIdToAssetGidSharedByCache[triple.subject]!.append(triple.object)
+            } else {
+                UserIdToAssetGidSharedByCache[triple.subject] = [triple.object]
+            }
         }
         
         return assetsToUsers
@@ -119,7 +163,26 @@ public enum SHKGQuery {
         var assetsToUsers = [GlobalIdentifier: Set<String>]()
         var sharedWithUsersCondition = KBTripleCondition(value: false)
         
+        var usersIdsToSearch = Set<String>()
         for userId in userIdentifiers {
+            if let assetIdsSharedWith = UserIdToAssetGidSharedWithCache[userId] {
+                assetIdsSharedWith.forEach({
+                    if assetsToUsers[$0] == nil {
+                        assetsToUsers[$0] = [userId]
+                    } else {
+                        assetsToUsers[$0]!.insert(userId)
+                    }
+                })
+            } else {
+                usersIdsToSearch.insert(userId)
+            }
+        }
+        
+        guard usersIdsToSearch.count > 0 else {
+            return assetsToUsers
+        }
+        
+        for userId in usersIdsToSearch {
             sharedWithUsersCondition = sharedWithUsersCondition.or(
                 KBTripleCondition(
                     subject: nil,
@@ -135,6 +198,12 @@ public enum SHKGQuery {
                 assetsToUsers[assetId]!.insert(triple.object)
             } else {
                 assetsToUsers[assetId] = [triple.object]
+            }
+            
+            if let _ = UserIdToAssetGidSharedWithCache[triple.object] {
+                UserIdToAssetGidSharedWithCache[triple.object]!.append(triple.subject)
+            } else {
+                UserIdToAssetGidSharedWithCache[triple.object] = [triple.subject]
             }
         }
         
