e186083d684dbbed1be69f53038f852e382fb5eb
gennaro-safehill <gennaro@safehill.io>
2023-12-17 22:03:08 -0800

    Use fit, not fill

--- Sources/Safehill-Client/Extensions/PHAsset.swift
+++ Sources/Safehill-Client/Extensions/PHAsset.swift
@@ -150,7 +150,7 @@ public extension PHAsset {
 
         PHImageManager().requestImage(for: self,
                                       targetSize: targetSize,
-                                      contentMode: .aspectFill,
+                                      contentMode: .aspectFit,
                                       options: options) {
             image, _ in
             if let image = image {
