2c877fd903c6a2a053b5aed7f8d4450c3ca958f9
gennaro-safehill <gennaro@safehill.io>
2024-04-23 14:40:22 -0700

    Ability to initialize a ConversationThreadAssetDTO

--- Sources/Safehill-Client/Network/DTOs/ConversationThreadAssetDTO.swift
+++ Sources/Safehill-Client/Network/DTOs/ConversationThreadAssetDTO.swift
@@ -5,5 +5,12 @@ public struct ConversationThreadAssetDTO: Codable {
     public let addedByUserIdentifier: String
     public let addedAt: String
     public let groupId: String
+    
+    init(globalIdentifier: String, addedByUserIdentifier: String, addedAt: String, groupId: String) {
+        self.globalIdentifier = globalIdentifier
+        self.addedByUserIdentifier = addedByUserIdentifier
+        self.addedAt = addedAt
+        self.groupId = groupId
+    }
 }
 
