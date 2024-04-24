cacce84fac09151719385a8a81e0e10387bafde5
gennaro-safehill <gennaro@safehill.io>
2024-04-23 17:02:54 -0700

    Bugfix on thread list query

--- Sources/Safehill-Client/Network/LocalServer.swift
+++ Sources/Safehill-Client/Network/LocalServer.swift
@@ -1464,6 +1464,8 @@ struct LocalServer : SHServerAPI {
             condition = KBGenericCondition(.beginsWith, value: "\(SHInteractionAnchor.thread.rawValue)::")
         }
         
+        condition.and(KBGenericCondition(.contains, value: "::assets::", negated: true))
+        
         let kvPairs: KBKVPairs
         do {
             kvPairs = try userStore
