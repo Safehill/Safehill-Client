4903bc583786653725ba02d7fb42f293e72b26b9
gennaro-safehill <gennaro@safehill.io>
2024-07-24 03:09:44 -0700

    Allow clients to construct BackedUpAsset objects

--- Sources/Safehill-Client/Models/Assets/BackedUpAsset.swift
+++ Sources/Safehill-Client/Models/Assets/BackedUpAsset.swift
@@ -4,4 +4,9 @@ import Photos
 public struct BackedUpAsset {
     public let phAsset: PHAsset
     public let globalIdentifier: String
+    
+    public init(phAsset: PHAsset, globalIdentifier: String) {
+        self.phAsset = phAsset
+        self.globalIdentifier = globalIdentifier
+    }
 }
