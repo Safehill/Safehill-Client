587c613aa5778585ba0e44f250cf16747a62a937
gennaro-safehill <gennaro@safehill.io>
2024-08-07 05:36:55 -0700

    Return nil if activity with assets removed can't be constructed

--- Sources/Safehill-Client/Models/UserActivities/AssetActivity.swift
+++ Sources/Safehill-Client/Models/UserActivities/AssetActivity.swift
@@ -1,6 +1,12 @@
 import Foundation
 
 public protocol AssetActivity: ReadableAssetActivity {
+    
     var requestedAt: Date { get }
-    func withAssetsRemoved(ids: [String]) -> any AssetActivity
+    
+    /// Returns an copy of the activity with the assets removed.
+    /// Use this method to keep assets in the activity immutable.
+    /// - Parameter ids: the asset ids to remove
+    /// - Returns: `nil` if no assets remain, the new activity otherwise
+    func withAssetsRemoved(ids: [String]) -> (any AssetActivity)?
 }
