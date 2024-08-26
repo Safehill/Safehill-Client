b1134ab3764d65a0683878ff0f8663e4858af63c
gennaro-safehill <gennaro@safehill.io>
2024-08-26 14:48:04 -0700

    Crash from debug log

--- Sources/Safehill-Client/Network/LocalServer.swift
+++ Sources/Safehill-Client/Network/LocalServer.swift
@@ -1023,7 +1023,7 @@ struct LocalServer : SHServerAPI {
             assetCondition = c
         }
         
-        log.debug("feching local server asset versions \(versions.map({ $0.rawValue })) chunk \(index) \(String(describing: assetIdentifiersChunk[index]))")
+        log.debug("feching local server asset versions \(versions.map({ $0.rawValue })) chunk \(index) \(String(describing: assetIdentifiersChunk))")
         
         assetStore.dictionaryRepresentation(forKeysMatching: prefixCondition.and(assetCondition)) {
             (result: Result) in
