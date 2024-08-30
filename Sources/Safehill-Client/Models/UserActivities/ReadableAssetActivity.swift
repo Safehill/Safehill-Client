8a547ff41cd2b2407ed1a9d55aa7694a766f16b8
gennaro-safehill <gennaro@safehill.io>
2024-08-30 10:06:51 -0700

    Add invitations info to ReadableAssetActivity

--- Sources/Safehill-Client/Models/UserActivities/ReadableAssetActivity.swift
+++ Sources/Safehill-Client/Models/UserActivities/ReadableAssetActivity.swift
@@ -5,6 +5,7 @@ public protocol ReadableAssetActivity: Hashable, Equatable, Identifiable {
     var groupId: String { get }
     var eventOriginator: any SHServerUser { get }
     var shareInfo: [(with: any SHServerUser, at: Date)] { get }
+    var invitationsInfo: [(with: String, at: Date)] { get }
 }
 
 
@@ -13,12 +14,14 @@ public extension ReadableAssetActivity {
         Set(lhs.assetIds) == Set(rhs.assetIds)
         && lhs.groupId == rhs.groupId
         && Set(lhs.shareInfo.map({ $0.with.identifier })) == Set(rhs.shareInfo.map({ $0.with.identifier }))
+        && Set(lhs.invitationsInfo.map({ $0.with })) == Set(rhs.invitationsInfo.map({ $0.with }))
     }
     
     func hash(into hasher: inout Hasher) {
         hasher.combine(assetIds)
         hasher.combine(groupId)
         hasher.combine(shareInfo.map { $0.with.identifier })
+        hasher.combine(invitationsInfo.map { $0.with })
     }
     
     var id: String {
