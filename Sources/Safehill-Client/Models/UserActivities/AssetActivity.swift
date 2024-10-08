8cc24c95432992f6d8322d987c9c1bf1d63e27c4
gennaro-safehill <gennaro@safehill.io>
2024-10-07 18:47:46 -0700

    Ability to remove phone numbers from activity

--- Sources/Safehill-Client/Models/UserActivities/AssetActivity.swift
+++ Sources/Safehill-Client/Models/UserActivities/AssetActivity.swift
@@ -9,4 +9,10 @@ public protocol AssetActivity: ReadableAssetActivity {
     /// - Parameter ids: the asset ids to remove
     /// - Returns: `nil` if no assets remain, the new activity otherwise
     func withAssetsRemoved(ids: [String]) -> (any AssetActivity)?
+    
+    /// Returns an copy of the activity with the invited phone numbers removed.
+    /// Use this method to keep assets in the activity immutable.
+    /// - Parameter phoneNumbers: the asset ids to remove
+    /// - Returns: `nil` if no assets remain, the new activity otherwise
+    func withPhoneNumbersRemoved(_ phoneNumbers: [String]) -> any AssetActivity
 }
