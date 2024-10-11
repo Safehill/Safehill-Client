7e8ca0d60de22d984c5e133c5bf4e315432bf652
gennaro-safehill <gennaro@safehill.io>
2024-10-10 23:01:25 -0700

    Keep references to both local and global identifier in activity as needed

--- Sources/Safehill-Client/Models/UserActivities/ReadableAssetActivity.swift
+++ Sources/Safehill-Client/Models/UserActivities/ReadableAssetActivity.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public protocol ReadableAssetActivity: Hashable, Equatable, Identifiable {
-    var assetIds: [String] { get }
+    var assetIds: [any GenericAssetIdentifiable] { get }
     var groupId: String { get }
     var eventOriginator: any SHServerUser { get }
     var shareInfo: [(with: any SHServerUser, at: Date)] { get }
@@ -11,14 +11,14 @@ public protocol ReadableAssetActivity: Hashable, Equatable, Identifiable {
 
 public extension ReadableAssetActivity {
     static func == (lhs: Self, rhs: Self) -> Bool {
-        Set(lhs.assetIds) == Set(rhs.assetIds)
+        Set(lhs.assetIds.map({ $0.hashValue })) == Set(rhs.assetIds.map({ $0.hashValue }))
         && lhs.groupId == rhs.groupId
         && Set(lhs.shareInfo.map({ $0.with.identifier })) == Set(rhs.shareInfo.map({ $0.with.identifier }))
         && Set(lhs.invitationsInfo.map({ $0.with })) == Set(rhs.invitationsInfo.map({ $0.with }))
     }
     
     func hash(into hasher: inout Hasher) {
-        hasher.combine(assetIds)
+        hasher.combine(assetIds.map({ $0.hashValue }))
         hasher.combine(groupId)
         hasher.combine(shareInfo.map { $0.with.identifier })
         hasher.combine(invitationsInfo.map { $0.with })
