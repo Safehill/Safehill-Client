6537ed15588ceec53821bda1fb975dc873f0ffb6
gennaro-safehill <gennaro@safehill.io>
2024-10-15 12:46:47 -0700

    Revisiting ReadableAssetActivity  Hashable

--- Sources/Safehill-Client/Models/UserActivities/ReadableAssetActivity.swift
+++ Sources/Safehill-Client/Models/UserActivities/ReadableAssetActivity.swift
@@ -11,14 +11,14 @@ public protocol ReadableAssetActivity: Hashable, Equatable, Identifiable {
 
 public extension ReadableAssetActivity {
     static func == (lhs: Self, rhs: Self) -> Bool {
-        Set(lhs.assetIds.map({ $0.hashValue })) == Set(rhs.assetIds.map({ $0.hashValue }))
+        Set(lhs.assetIds) == Set(rhs.assetIds)
         && lhs.groupId == rhs.groupId
         && Set(lhs.shareInfo.map({ $0.with.identifier })) == Set(rhs.shareInfo.map({ $0.with.identifier }))
         && Set(lhs.invitationsInfo.map({ $0.with })) == Set(rhs.invitationsInfo.map({ $0.with }))
     }
     
     func hash(into hasher: inout Hasher) {
-        hasher.combine(assetIds.map({ $0.hashValue }))
+        hasher.combine(assetIds)
         hasher.combine(groupId)
         hasher.combine(shareInfo.map { $0.with.identifier })
         hasher.combine(invitationsInfo.map { $0.with })
