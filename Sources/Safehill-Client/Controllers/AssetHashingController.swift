0d709d136d4b4e1647435681ebfe1f413c6872fd
gennaro-safehill <gennaro@safehill.io>
2024-11-21 16:45:47 -0800

    Hash distance

---
+++ Sources/Safehill-Client/Controllers/AssetHashingController.swift
@@ -0,0 +1,96 @@
+import CocoaImageHashing
+import Photos
+import Safehill_Crypto
+
+public typealias SHAssetPerceptualHash = String
+public typealias SHPerceptualHashDistance = Int64
+
+public class SHHashingController {
+    
+    static let HashingProvider = OSImageHashingProviderId.pHash
+    
+    public enum Error: Swift.Error, CustomStringConvertible, LocalizedError {
+        case noImageData
+        case invalidHash
+        
+        public var description: String {
+            switch self {
+            case .noImageData:
+                return "No data to calculate the hash"
+            case .invalidHash:
+                return "The providede hash is invalid"
+            }
+        }
+    }
+    
+    static func globalIdentifier(
+        for photoAsset: SHApplePhotoAsset
+    ) async -> GlobalIdentifier {
+        let exifData = await photoAsset.getExifData()
+        
+        /// Global identifier is generated based on a combination of:
+        /// - media type and subtype (metadata)
+        /// - width and height (metadata)
+        /// - datetime it was taken (metadata)
+        /// - location (metadata)
+        /// - EXIF data, like aperture, shutterspeed, and iso (metadata)
+        ///
+        /// If any metadata such as datetime, location and EXIF data of the photo is missing
+        /// a UUID generated each time the identifier is generated is added for randomness.
+        /// If all the metadata values are present, then photos are equal when
+        /// - taken at a specific time (with milliseconds)
+        /// - in a specific location (with exact coordinates)
+        /// - with the specific settings (width, height, media type, EXIF data)
+        ///
+        
+        var seed = [
+            "\(photoAsset.phAsset.mediaType.rawValue)",
+            "\(photoAsset.phAsset.mediaSubtypes.rawValue)",
+            "\(photoAsset.phAsset.pixelWidth)",
+            "\(photoAsset.phAsset.pixelHeight)"
+        ]
+        
+        if photoAsset.phAsset.creationDate == nil || photoAsset.phAsset.location == nil || exifData == nil {
+            seed.append(UUID().uuidString)
+        } else {
+            seed.append(photoAsset.phAsset.creationDate!.iso8601withFractionalSeconds)
+            seed.append("\(photoAsset.phAsset.location!.coordinate.latitude)")
+            seed.append("\(photoAsset.phAsset.location!.coordinate.longitude)")
+            seed.append("\(exifData!.aperture)+\(exifData!.shutterSpeed)+\(exifData!.iso)")
+        }
+        
+        let globalIdentifier = SHHash.stringDigest(for: seed.joined(separator: ":").data(using: .utf8)!)
+        return globalIdentifier
+    }
+    
+    static func perceptualHash(forImageData imageData: Data) throws -> SHAssetPerceptualHash {
+        guard imageData.count > 0 else {
+            throw Error.noImageData
+        }
+        let osHash = OSImageHashing.sharedInstance().hashImageData(
+            imageData,
+            with: HashingProvider
+        )
+        if osHash == OSHashTypeError {
+            throw Error.noImageData
+        }
+        return "\(osHash)"
+    }
+    
+    static func calculateDistanceBetween(
+        lhsPerceptualHash: SHAssetPerceptualHash,
+        rhsPerceptualHash: SHAssetPerceptualHash
+    ) throws -> OSHashDistanceType {
+        guard let lhs = OSHashType(lhsPerceptualHash),
+              let rhs = OSHashType(rhsPerceptualHash)
+        else {
+            throw SHHashingController.Error.invalidHash
+        }
+        
+        return Int64(OSImageHashing.sharedInstance().hashDistance(
+            lhs,
+            to: rhs,
+            with: HashingProvider
+        ))
+    }
+}
--- Tests/Safehill-ClientTests/AssetHashingTests.swift
+++ Tests/Safehill-ClientTests/AssetHashingTests.swift
@@ -3,7 +3,7 @@ import XCTest
 
 final class Safehill_AssetHashingTests: XCTestCase {
     
-    func testPerceptualHashing() async throws {
+    func testIdempotency() async throws {
         guard let data = Data(base64Encoded: SmallTestImageData) else {
             XCTFail()
             return
@@ -11,6 +11,27 @@ final class Safehill_AssetHashingTests: XCTestCase {
         let perceptualHash = try SHHashingController.perceptualHash(
             forImageData: data
         )
-        XCTAssertEqual(perceptualHash, "")
+        XCTAssertEqual(perceptualHash, "-378606956254134784")
+        
+        let sameHash = try SHHashingController.perceptualHash(
+            forImageData: data
+        )
+        XCTAssertEqual(perceptualHash, sameHash)
+        
+        guard let largeData = Data(base64Encoded: LargeTestImageDataEncoded) else {
+            XCTFail()
+            return
+        }
+        let largeImagePerceptualHash = try SHHashingController.perceptualHash(
+            forImageData: largeData
+        )
+        XCTAssertNotEqual(perceptualHash, largeImagePerceptualHash)
+        
+        let distance = try SHHashingController.calculateDistanceBetween(
+            lhsPerceptualHash: perceptualHash,
+            rhsPerceptualHash: largeImagePerceptualHash
+        )
+        
+        XCTAssertEqual(distance, 22)
     }
 }
