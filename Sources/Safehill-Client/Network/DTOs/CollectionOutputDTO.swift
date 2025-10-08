e0ccc5b2ca750736c4ca4782a0d132af5be7fa1f
gennaro-safehill <gennaro@safehill.io>
2025-10-08 10:06:16 -0700

    feat: adding isSystemCollection in CollectionOutputDTO

--- Sources/Safehill-Client/Network/DTOs/CollectionOutputDTO.swift
+++ Sources/Safehill-Client/Network/DTOs/CollectionOutputDTO.swift
@@ -4,6 +4,7 @@ public struct CollectionOutputDTO: Codable {
     public let id: String
     public let name: String
     public let description: String
+    public let isSystemCollection: Bool
     public let assetCount: Int
     public let visibility: String
     public let pricing: Double
