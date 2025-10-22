f33b96087401784484ec9904f5b87fae25dc2065
gennaro-safehill <gennaro@safehill.io>
2025-10-22 10:37:29 -0700

    Public initializer for AccessCheckResultDTO

--- Sources/Safehill-Client/Network/DTOs/AccessCheckResultDTO.swift
+++ Sources/Safehill-Client/Network/DTOs/AccessCheckResultDTO.swift
@@ -6,4 +6,12 @@ public struct AccessCheckResultDTO: Codable {
     public let price: Double?
     public let visibility: String?
     public let createdBy: String?
+    
+    public init(status: String, message: String?, price: Double?, visibility: String?, createdBy: String?) {
+        self.status = status
+        self.message = message
+        self.price = price
+        self.visibility = visibility
+        self.createdBy = createdBy
+    }
 }
