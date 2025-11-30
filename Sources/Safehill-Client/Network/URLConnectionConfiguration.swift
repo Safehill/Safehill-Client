83b9e810f04ff5444b37dadfdc9c8597fa401fe1
gennaro-safehill <gennaro@safehill.io>
2025-11-29 17:58:10 -0800

    Updating ngrok url

--- Sources/Safehill-Client/Network/URLConnectionConfiguration.swift
+++ Sources/Safehill-Client/Network/URLConnectionConfiguration.swift
@@ -16,7 +16,8 @@ public let SafehillServerURLComponents: URLComponents = {
     components.port = 8080
 #elseif DEBUG
     components.scheme = "https"
-    components.host = "safehill-stage-1-ec0cd53b3592.herokuapp.com"
+    components.host = "079bad5ce6d0.ngrok-free.app"
+//    components.host = "safehill-stage-1-ec0cd53b3592.herokuapp.com"
     components.port = 443
 #else
     components.scheme = "https"
@@ -36,7 +37,8 @@ public let SafehillServerURLComponentsForWebsockets: URLComponents = {
     components.port = 8080
 #elseif DEBUG
     components.scheme = "wss"
-    components.host = "safehill-stage-1-ec0cd53b3592.herokuapp.com"
+    components.host = "079bad5ce6d0.ngrok-free.app"
+//    components.host = "safehill-stage-1-ec0cd53b3592.herokuapp.com"
     components.port = 443
 #else
     components.scheme = "wss"
